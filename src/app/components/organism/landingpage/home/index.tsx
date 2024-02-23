818063c275f9ce03cfebf3e5bf0f4868baab09f5
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-02-23 08:03:16 +0700

    finish all home

--- src/app/components/organism/landingpage/home/index.tsx
+++ src/app/components/organism/landingpage/home/index.tsx
@@ -4,10 +4,8 @@ import React from 'react'
 import './home.scss'
 import { motion } from "framer-motion";
 import { slideIn, staggerContainer, textVariants } from '@/app/utils/motion'
-import { Hurricane } from 'next/font/google';
 import { homeIcon } from '@/app/image';
 
-const hurricane = Hurricane({ weight: '400', subsets: ['latin'] });
 const Home = () => {
 
     return (
@@ -20,7 +18,7 @@ const Home = () => {
                 <motion.div className="title grid items-center justify-start p-2"
                     variants={slideIn("left", "tween", 0.1, 1)}>
                     <motion.p variants={textVariants(1.3)}><i>Life is an event</i></motion.p>
-                    <motion.h1 className={hurricane.className} variants={textVariants(1.1)}>WEBSITE UNDANGAN DIGITAL</motion.h1>
+                    <motion.h1 variants={textVariants(1.1)}>WEBSITE UNDANGAN DIGITAL</motion.h1>
                     <motion.h2 variants={textVariants(1.2)}>Rayakan pernikahan anda bersama orang-orang terdekat dalam momen istimewa! Ajak mereka dengan cara istimewa dan unik. Coba sekarang, GRATIS!</motion.h2>
                 </motion.div>
 
