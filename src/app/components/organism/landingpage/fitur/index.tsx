37e3d0d3dd015226d61ddc76faf89a1ca103ab80
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-02-26 10:52:30 +0700

    setup fitur page

---
+++ src/app/components/organism/landingpage/fitur/index.tsx
@@ -0,0 +1,9 @@
+import './fitur.scss'
+
+const Fitur = () => {
+    return (
+        <div>Fitur</div>
+    )
+}
+
+export default Fitur
--- src/app/view/LandingPage/LandingPageView.tsx
+++ src/app/view/LandingPage/LandingPageView.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/app/components/organism/landingpage/navbar"
 import Home from "@/app/components/organism/landingpage/home"
 import Instruction from "@/app/components/organism/landingpage/instruction"
 import Theme from "@/app/components/organism/landingpage/theme"
+import Fitur from "@/app/components/organism/landingpage/fitur"
 
 const LandingPageView = () => {
     return (
@@ -10,6 +11,7 @@ const LandingPageView = () => {
             {/* <Home /> */}
             <Instruction />
             <Theme />
+            <Fitur />
         </>
     )
 }
