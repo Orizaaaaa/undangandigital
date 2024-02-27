76a7610f7019b67c3993d066eb3e9c305013e49a
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-02-27 11:13:23 +0700

    finish payment page

--- src/app/components/organism/landingpage/paymentMethod/index.tsx
+++ src/app/components/organism/landingpage/paymentMethod/index.tsx
@@ -6,9 +6,9 @@ const PaymentMethod = () => {
     return (
         <section className="payment-method mt-[6rem] mb-4" id="payment-method">
             <h1 className="text-center font-semibold" >Metode Pembayaran Yang Tersedia Untuk Anda</h1>
-            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-3 mt-8">
+            <div className="grid grid-cols-5  gap-3 mt-8 p-3">
                 {paymentList.map((element, index) => (
-                    <Image key={index} src={element} alt="payment method" />
+                    <Image className="mx-auto my-auto" key={index} src={element} alt="payment method" />
                 ))}
             </div>
 
