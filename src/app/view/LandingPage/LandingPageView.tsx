95e9549c4b3f324e5dacf4857f9182ba832c6fbc
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-03-04 06:37:57 +0700

    create accordion page

--- src/app/view/LandingPage/LandingPageView.tsx
+++ src/app/view/LandingPage/LandingPageView.tsx
@@ -6,6 +6,7 @@ import Fitur from "@/app/components/organism/landingpage/fitur"
 import Price from "@/app/components/organism/landingpage/price"
 import PaymentMethod from "@/app/components/organism/landingpage/paymentMethod"
 import Testimonies from "@/app/components/organism/landingpage/testimonies"
+import Accordion from "@/app/components/organism/landingpage/faq"
 
 const LandingPageView = () => {
     return (
@@ -18,6 +19,7 @@ const LandingPageView = () => {
             <Price />
             <PaymentMethod />
             <Testimonies />
+            <Accordion />
         </>
     )
 }
