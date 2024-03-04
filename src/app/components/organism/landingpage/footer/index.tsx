18f196f5b5bbceaad1873b73d2e075c2afd5d278
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-03-04 08:45:23 +0700

    finish footer landing page

--- src/app/components/organism/landingpage/footer/index.tsx
+++ src/app/components/organism/landingpage/footer/index.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import './footer.scss'
+import Link from 'next/link'
+
 
 const Footer = () => {
     return (
-        <section className="footer" id="footer">
-            <div className="container mx-auto">
-                <p>pantek</p>
+        <section className='footer mt-[6rem]' id="footer" >
+            <div className="container   mx-auto py-[4rem]">
+                <div className="grid grid-cols-1 lg:grid-cols-2 ">
+                    <div className="left p-3 sm:p-0">
+                        <h1>OUR WEDDING INVITATION WILL LEAVE YOU BREATHLESS ON YOUR SPECIAL DAY</h1>
+                        <h2 className='text-lg font-semibold' >Copyright ©Pasti Dateng.</h2>
+                    </div>
+                    <div className="right mx-0 mt-3 lg:mt-0 lg:mx-auto p-3 sm:p-0">
+                        <h1 className='text-xl font-semibold'>SITEMAP</h1>
+                        <div className="flex gap-5">
+                            <Link className='text-lg font-semibold' href={'#home'} > Home</Link>
+                            <Link className='text-lg font-semibold' href={'#home'} > Home</Link>
+                            <Link className='text-lg font-semibold' href={'#home'} > Home</Link>
+                            <Link className='text-lg font-semibold' href={'#home'} > Home</Link>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     )
