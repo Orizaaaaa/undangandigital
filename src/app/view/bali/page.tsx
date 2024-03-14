5f749b1815a466caa107ce3b1ba8a528cd73b0c3
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-03-14 20:27:06 +0700

    create hero 1

--- src/app/view/bali/page.tsx
+++ src/app/view/bali/page.tsx
@@ -1,10 +1,15 @@
 import LayoutTheme from "@/app/components/fragments/layout"
 import { userBali1 } from "@/app/image"
+import './bali.scss'
 
 const BaliTheme = () => {
     return (
         <LayoutTheme image={userBali1} >
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea nam natus officia voluptatum omnis voluptas, deleniti dolorum nisi facilis molestiae ratione? Ullam quos delectus quibusdam vitae error officia porro unde.</p>
+            <section className="bali-theme">
+                <div className="hero-1">
+
+                </div>
+            </section>
         </LayoutTheme>
     )
 }
