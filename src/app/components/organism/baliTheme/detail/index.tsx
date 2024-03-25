01df8e27cfb5938aeceaad4a52ea504a43d53cb9
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-03-25 08:08:36 +0700

    aman

--- src/app/components/organism/baliTheme/detail/index.tsx
+++ src/app/components/organism/baliTheme/detail/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import './index.scss'
 import Image from 'next/image'
-import { bgDecor, introIntivitation } from '@/app/image'
+import { bgDecor, introIntivitation, userBali1 } from '@/app/image'
 
 const DetailInfo = () => {
     return (
         <div className="hero-3-bali flex flex-col items-center justify-center px-4 pt-32 ">
-            <div className="image-section p-[74px]">
-                <Image src={introIntivitation} alt='decor' className='h-52 w-52 border-4 border-yellow-900 rounded-full p-0 m-0' />
+            <div className="image-section p-[120px]">
+                <div>
+                    <Image src={introIntivitation} alt='decor' className=' max-h-[300px] w-[100vw] border-4 border-yellow-900 rounded-full p-0 m-0 object-cover' />
+                </div>
             </div>
-
         </div>
     )
 }
