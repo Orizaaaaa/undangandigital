34672ee7250e8acebe33ef0b14b3644987abbe01
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-03-25 22:26:53 +0700

    fixing bug

--- src/app/components/organism/baliTheme/messages/index.tsx
+++ src/app/components/organism/baliTheme/messages/index.tsx
@@ -1,8 +1,12 @@
+import { playballFont } from '@/app/utils/font'
 import './index.scss'
 const Messages = () => {
     return (
         <section className="messages">
-            <p>lore</p>
+            <div className="flex flex-col justify-center items-center p-3 shadow-black">
+                <h1 className='text-3xl' style={{ fontFamily: playballFont.style.fontFamily }} >Ucapan & Doa</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat ipsam in sunt iusto architecto suscipit corrupti aspernatur minima, ab velit labore vero autem. Recusandae, aut. Cupiditate laborum magnam ipsam eos?</p>
+            </div>
         </section>
     )
 }
