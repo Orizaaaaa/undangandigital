a99c8be75d9ed2990837154e8d41978e87e829bc
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2024-03-25 08:30:42 +0700

    finish bug detailinfo

--- src/app/components/organism/baliTheme/detail/index.tsx
+++ src/app/components/organism/baliTheme/detail/index.tsx
@@ -6,7 +6,7 @@ import { bgDecor, introIntivitation, userBali1 } from '@/app/image'
 const DetailInfo = () => {
     return (
         <div className="hero-3-bali flex flex-col items-center justify-center px-4 pt-32 ">
-            <div className="image-section p-[120px]">
+            <div className="image-section-bali p-[120px]">
                 <div>
                     <Image src={introIntivitation} alt='decor' className=' max-h-[300px] w-[100vw] border-4 border-yellow-900 rounded-full p-0 m-0 object-cover' />
                 </div>
